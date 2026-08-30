afcd02782fec7b1a59fd716d1c9b4d15204922fc
dvfens <aryadivyansh01122006@gmail.com>
2026-08-30 08:09:09 +0530

    Initial commit

---
+++ conditionals.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class conditionals{
+    public static void main(String args[]){
+       Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if(a>90){
+            System.out.println("Excellent");
+        }
+        else if(a>80){
+            System.out.println("Very Good");
+        }
+        else if(a>60){
+            System.out.println("Good");
+        }
+        else if(a>45){
+            System.out.println("Average");
+        }
+        else if(a>33){
+            System.out.println("Can do better");
+        }
+        else{
+            System.out.println("work harder");
+        }
+    }
+}
